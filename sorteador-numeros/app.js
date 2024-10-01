d1e65a975b94f16954f87609afa7a77a0551288b
David T. <davidt.info@gmail.com>
2024-10-01 17:57:26 -0300

    proteção na entrada de valores

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,6 +2,12 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+    let resultado = document.getElementById('resultado');
+
+    if(de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
 
     let sorteados = [];
     let numero;
@@ -16,7 +22,7 @@ function sortear(){
         sorteados.push(numero);
     }
 
-    let resultado = document.getElementById('resultado');
+    
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
     alterarStatusBotao();
 
