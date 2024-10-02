b88f98ff588c8c31a02f15450d30dade8bacfbc0
David T. <davidt.info@gmail.com>
2024-10-02 17:57:32 -0300

    carrinho-compras: Calculando valor total do carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,3 +1,6 @@
+let totalGeral;
+limpar();
+
 function adicionar(){
     //recuperar valores nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
@@ -11,43 +14,18 @@ function adicionar(){
     carrinho.innerHTML += `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`;
-
+    //atualizar o valor total
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 
 }
 
 function limpar(){
+    
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0'
 
 }
-
-//Codigo proprio
-// function adicionar(){
-//     let produto = document.getElementById(`produto`);
-//     let produtoNome = produto.value.split('-')[0].trim();
-//     let produtoValor = produto.value.split('-')[1].trim().replace('R$', '');
-//     let quantidade = document.getElementById(`quantidade`).value;
-//     let carrinhoProdutos = document.getElementById(`lista-produtos`);
-//     let valorTotal = document.getElementById(`valor-total`);
-//     let calculoValorTotal = parseFloat(valorTotal.innerHTML.replace('R$', ''));
-
-//     if(quantidade == ''){
-//         alert('Informe uma quantidade!');
-//         return;
-//     }else {
-//         quantidade = parseInt(quantidade);
-//         calculoValorTotal += parseFloat(quantidade) * parseFloat(produtoValor); 
-//         valorTotal.innerHTML = 'R$'+calculoValorTotal;
-//         console.log(calculoValorTotal);
-//     }
-
-//     let novoProduto = document.createElement('section');
-//     novoProduto.className = 'carrinho__produtos__produto';
-//     novoProduto.innerHTML = `<span class="texto-azul">${quantidade}x</span> ${produtoNome} <span class="texto-azul">R$${produtoValor}</span>`;
-
-//     carrinhoProdutos.appendChild(novoProduto);
-
-// }
-
-// function limpar() {
-//     document.getElementById(`lista-produtos`).innerHTML = '';
-//     document.getElementById(`valor-total`).innerHTML = 'R$0';
-// }
