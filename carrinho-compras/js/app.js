53ef5b2ff5a037c36831f0ffb8eeb9ff92253b90
David T. <davidt.info@gmail.com>
2024-10-02 17:42:28 -0300

    carrinho-compras: Calculando o valor do produto

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,7 +1,14 @@
 function adicionar(){
     //recuperar valores nome do produto, quantidade e valor
-    let nomeProduto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
+    let preco = quantidade.value * valorUnitario;
+    alert(nomeProduto);
+    alert(valorUnitario);
+    alert(quantidade.value);
+    alert(preco);
     
 
 }
