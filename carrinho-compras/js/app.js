0fb160825a3a77485b2ae893d6b634d62cd34e70
David T. <davidt.info@gmail.com>
2024-10-02 17:49:05 -0300

    carrinho-compras: Adicionando produto no carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -3,13 +3,15 @@ function adicionar(){
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    let preco = quantidade.value * valorUnitario;
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    alert(preco);
-    
+    let quantidade = document.getElementById('quantidade').value;
+    //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    //adicionar no carrinho
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+
 
 }
 
